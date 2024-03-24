43be66bb3c165f8fd391065684f46f0c0d809e1c
MasumaJaffery <masumajaffery1@gmail.com>
2024-03-24 19:50:25 +0500

    Intro de Python

---
+++ 1.py
@@ -0,0 +1,28 @@
+print("Happy Coding!")
+# Variables Types
+print(type(3.14))
+print(type(2))
+print(type("Hello"))
+# Variables
+city = "Paris"
+print(city)
+# Build-in Functions
+print(len("BlahBlahBlah"))
+name = "guriya"
+print(name.capitalize());
+print(name.upper());
+# Conditional Statements
+if name.endswith("a"):
+    print("Yes")
+else:
+    print("No")
+a = 10
+b = 10
+c = 20
+if a == b:
+  print("Equals")
+elif a + b != c:
+   print("Not to be Sum")
+else:
+  print("Not Equals")
+  
